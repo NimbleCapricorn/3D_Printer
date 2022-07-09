a1166d6472e5676c4d62579aaa150438e8e16047
Nimble Capricorn <Nimble.capricorn@gmail.com>
2022-07-09 14:54:53 +0200

    .

--- src/main.py
+++ src/main.py
@@ -2,11 +2,22 @@ from typing import Union
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-
+from starlette.responses import FileResponse
 
 app = FastAPI()
 app.mount("/", StaticFiles(directory="static", html = True), name="static")
 
+@app.get("/print")
+async def read_print_page():
+    return FileResponse('static/print.html')
+
+@app.get("/config")
+async def read_print_page():
+    return FileResponse('static/config.html')
+
+@app.get("/change_fillament")
+async def read_print_page():
+    return FileResponse('static/change_fillament.html')
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):

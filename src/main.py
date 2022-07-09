9318c318d07332b7e52fbe7425a3491e119cf311
Nimble Capricorn <Nimble.capricorn@gmail.com>
2022-07-09 15:36:07 +0200

    .

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from typing import Union
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from starlette.responses import FileResponse
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 app.mount("/", StaticFiles(directory="static", html = True), name="static")
@@ -10,12 +12,6 @@ app.mount("/", StaticFiles(directory="static", html = True), name="static")
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
-
-
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import HTMLResponse
-
-app = FastAPI()
 
 @app.post("/uploadfiles/")
 async def create_upload_files(

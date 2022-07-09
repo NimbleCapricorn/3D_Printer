dc619962ef75e89c3047b6dc63020e8dce4fc917
Nimble Capricorn <Nimble.capricorn@gmail.com>
2022-07-09 14:58:20 +0200

    .

--- src/main.py
+++ src/main.py
@@ -7,18 +7,6 @@ from starlette.responses import FileResponse
 app = FastAPI()
 app.mount("/", StaticFiles(directory="static", html = True), name="static")
 
-@app.get("/print")
-async def read_print_page():
-    return FileResponse('static/print.html')
-
-@app.get("/config")
-async def read_config_page():
-    return FileResponse('static/config.html')
-
-@app.get("/change_fillament")
-async def read_change_fillament_page():
-    return FileResponse('static/change_fillament.html')
-
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}

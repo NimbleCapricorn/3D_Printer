f6981a77a5373094ab5dd633bcc5556b12d2187b
Nimble Capricorn <Nimble.capricorn@gmail.com>
2022-07-09 16:04:00 +0200

    .

--- src/main.py
+++ src/main.py
@@ -5,7 +5,8 @@ from starlette.responses import FileResponse
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-templates = Jinja2Templates(directory="templates")
+
+templates = Jinja2Templates(directory="static")
 
 app = FastAPI()
 
@@ -18,5 +19,17 @@ async def create_upload_files(
     files: list[UploadFile] = File(description="Multiple files as UploadFile"),
 ):
     return {"filenames": [file.filename for file in files]}
+
+@app.get("/print", response_class=HTMLResponse)
+async def read_print_page():
+    return templates.TemplateResponse("print.html")
+
+@app.get("/config", response_class=HTMLResponse)
+async def read_config_page():
+    return templates.TemplateResponse("config.html")
+
+@app.get("/change_fillament", response_class=HTMLResponse)
+async def read_change_fillament_page():
+    return templates.TemplateResponse("change_fillament.html")
 
 app.mount("/", StaticFiles(directory="static", html = True), name="static")

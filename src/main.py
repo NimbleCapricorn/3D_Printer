99d54a17373941b21876c264b97ec78827fca293
Nimble Capricorn <Nimble.capricorn@gmail.com>
2022-07-09 15:30:46 +0200

    File upload

--- src/main.py
+++ src/main.py
@@ -5,8 +5,45 @@ from fastapi.staticfiles import StaticFiles
 from starlette.responses import FileResponse
 
 app = FastAPI()
-app.mount("/", StaticFiles(directory="static", html = True), name="static")
+# app.mount("/", StaticFiles(directory="static", html = True), name="static")
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+
+@app.post("/files/")
+async def create_files(
+    files: list[bytes] = File(description="Multiple files as bytes"),
+):
+    return {"file_sizes": [len(file) for file in files]}
+
+
+@app.post("/uploadfiles/")
+async def create_upload_files(
+    files: list[UploadFile] = File(description="Multiple files as UploadFile"),
+):
+    return {"filenames": [file.filename for file in files]}
+
+
+@app.get("/")
+async def main():
+    content = """
+<body>
+	<form action="/files/" enctype="multipart/form-data" method="post">
+		<input name="files" type="file" multiple>
+		<input type="submit">
+	</form>
+	<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+		<input name="files" type="file" multiple>
+		<input type="submit">
+	</form>
+</body>
+    """
+    return HTMLResponse(content=content)
